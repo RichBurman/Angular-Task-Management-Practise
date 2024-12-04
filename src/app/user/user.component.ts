b8af4eb1fa8a2af09890a93772a32cf2b660623f
Richard Burman <richard.burman@harperjames.co.uk>
2024-12-04 15:52:50 +0000

    Make avatar and name inputs required in user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,8 +11,8 @@ import { Component, computed, Input, signal } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
