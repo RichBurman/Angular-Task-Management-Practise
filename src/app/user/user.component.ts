a7ebcdcf2673bcfcdbf3352bb02f51168279ff4b
Richard Burman <richard.burman@harperjames.co.uk>
2024-12-05 16:03:31 +0000

    Refactor user component to accept user object and update template for improved clarity

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,9 +11,11 @@ import { Component, computed, Input, signal, input, Output, EventEmitter, output
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+@Input({required: true}) user!: {
+  id: string;
+  name: string;
+  avatar: string;
+}
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
@@ -27,7 +29,7 @@ export class UserComponent {
   // })
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
@@ -37,7 +39,7 @@ export class UserComponent {
   // }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // this.selectedUser = DUMMY_USERS[randomIndex];
