bce73c986cc2beb5f716650ea50cc5462bd0f550
Richard Burman <richard.burman@harperjames.co.uk>
2024-12-05 11:30:29 +0000

    Enhance user component to support user selection by adding an id input and emitting select event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal, input } from '@angular/core';
+import { Component, computed, Input, signal, input, Output, EventEmitter } from '@angular/core';
 
 // import { DUMMY_USERS } from '../dummy-users';
 
@@ -11,17 +11,20 @@ import { Component, computed, Input, signal, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
 
   // Below is how to use the signal and computed functions instead of the @Input decorator
+
   // avatar = input.required<string>();
   // name = input.required<string>();
 
   // imagePath = computed(() => {
   //   return 'assets/users/' + this.avatar();
   // })
-  
+
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
@@ -33,6 +36,7 @@ export class UserComponent {
   // }
 
   onSelectUser() {
+    this.select.emit(this.id);
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // this.selectedUser = DUMMY_USERS[randomIndex];
