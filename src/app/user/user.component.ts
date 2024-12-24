42119a2177fd80168d72804cb7e6a4048e7d9a7c
Richard Burman <richard.burman@harperjames.co.uk>
2024-12-24 10:58:23 +0000

    Refactor task and user components to utilize app-card for consistent styling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/core';
 
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 // type User = {
 //   id: string;
@@ -26,6 +27,7 @@ import { type User } from './user.model';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
